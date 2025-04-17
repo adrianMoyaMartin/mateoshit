70c9e814dcb9ee8124cf461fd1b39ee9b1391740
adrianMoyaMartin <adrian.moya.martin@gmail.com>
2025-04-17 14:23:57 +0200

    chore: fmt

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,31 @@ use species::{BehaviourArchetypes, Organism, Species};
 
 mod species;
 fn main() {
-    let (mut rl, thread) = raylib::init()
-        .size(640, 480)
-        .title("Hello, World")
-        .build();
+    let (mut rl, thread) = raylib::init().size(640, 480).title("Hello, World").build();
     rl.set_target_fps(30);
     let mut species = Species::new();
-    let org = Organism::new(10.0, 1.0, 1000, dvec2(300.0, 300.0), BehaviourArchetypes::Hunter);
-    let org2 = Organism::new(10.0, 1.0, 10, dvec2(100.0, 300.0), BehaviourArchetypes::Prey);
-    let org3 = Organism::new(10.0, 1.0, 10, dvec2(600.0, 300.0), BehaviourArchetypes::Prey);
+    let org = Organism::new(
+        10.0,
+        1.0,
+        1000,
+        dvec2(300.0, 300.0),
+        BehaviourArchetypes::Hunter,
+    );
+    let org2 = Organism::new(
+        10.0,
+        1.0,
+        10,
+        dvec2(100.0, 300.0),
+        BehaviourArchetypes::Prey,
+    );
+    let org3 = Organism::new(
+        10.0,
+        1.0,
+        10,
+        dvec2(600.0, 300.0),
+        BehaviourArchetypes::Prey,
+    );
 
-    
     species.add(org);
     species.add(org2);
     species.add(org3);
--- src/species.rs
+++ src/species.rs
@@ -1,30 +1,53 @@
-use glam::{dvec2, DVec2};
+use glam::{DVec2, dvec2};
 use rand::{self, Rng};
-use raylib::{color::Color, ffi::{Rectangle, Vector2}, prelude::{RaylibDraw, RaylibDrawHandle}};
+use raylib::{
+    color::Color,
+    ffi::{Rectangle, Vector2},
+    prelude::{RaylibDraw, RaylibDrawHandle},
+};
 
 #[derive(Debug, PartialEq, Clone)]
 pub struct Species {
-    organisms: Vec<Organism>
+    organisms: Vec<Organism>,
 }
 impl Species {
     pub fn new() -> Species {
-        Species {organisms: vec![]}
+        Species { organisms: vec![] }
     }
     pub fn draw(&self, d: &mut RaylibDrawHandle) {
         for object in self.organisms.iter() {
             match object.behaviour_type {
                 BehaviourArchetypes::Prey => {
-                    d.draw_rectangle_pro(Rectangle {x: object.pos.x as f32, y: object.pos.y as f32, width: 5.0, height: 5.0 },Vector2 {x:0.0, y: 0.0}, 0.0, Color::GREEN);
-
-                },
+                    d.draw_rectangle_pro(
+                        Rectangle {
+                            x: object.pos.x as f32,
+                            y: object.pos.y as f32,
+                            width: 5.0,
+                            height: 5.0,
+                        },
+                        Vector2 { x: 0.0, y: 0.0 },
+                        0.0,
+                        Color::GREEN,
+                    );
+                }
                 BehaviourArchetypes::Hunter => {
-                    d.draw_rectangle_pro(Rectangle {x: object.pos.x as f32, y: object.pos.y as f32, width: 5.0, height: 5.0 },Vector2 {x:0.0, y: 0.0}, 0.0, Color::RED);
-                },
+                    d.draw_rectangle_pro(
+                        Rectangle {
+                            x: object.pos.x as f32,
+                            y: object.pos.y as f32,
+                            width: 5.0,
+                            height: 5.0,
+                        },
+                        Vector2 { x: 0.0, y: 0.0 },
+                        0.0,
+                        Color::RED,
+                    );
+                }
             }
         }
     }
     pub fn update(&mut self) {
-        let n= self.organisms.len();
+        let n = self.organisms.len();
         for organism in &mut self.organisms {
             organism.movement();
             organism.clear_vision();
@@ -52,28 +75,38 @@ pub struct Organism {
     behaviour_type: BehaviourArchetypes,
     current_behaviour: CurrentBehaviour,
     pos: DVec2,
-    visible_creatures: Vec<DVec2>
+    visible_creatures: Vec<DVec2>,
 }
 impl Organism {
-    pub fn new(metabolism: f64, speed: f64, vision: i32, pos: DVec2, behaviour_type: BehaviourArchetypes ) -> Self {
-        Self { energy: 100.0, metabolism, speed, vision, pos, behaviour_type, current_behaviour: CurrentBehaviour::Idle, visible_creatures: vec![] }
+    pub fn new(
+        metabolism: f64,
+        speed: f64,
+        vision: i32,
+        pos: DVec2,
+        behaviour_type: BehaviourArchetypes,
+    ) -> Self {
+        Self {
+            energy: 100.0,
+            metabolism,
+            speed,
+            vision,
+            pos,
+            behaviour_type,
+            current_behaviour: CurrentBehaviour::Idle,
+            visible_creatures: vec![],
+        }
     }
     fn movement(&mut self) {
         let mut rng = rand::rng();
         let movement: f64 = rng.random();
-        
+
         match self.current_behaviour {
             CurrentBehaviour::Active(flee_dir) => {
                 match self.behaviour_type {
-                    BehaviourArchetypes::Prey => {
-                        self.pos -= flee_dir.normalize() * movement
-                    },
-                    BehaviourArchetypes::Hunter => {
-                        self.pos += flee_dir.normalize() * 1.0
-                    },
-                }
-                ;
-            },
+                    BehaviourArchetypes::Prey => self.pos -= flee_dir.normalize() * movement,
+                    BehaviourArchetypes::Hunter => self.pos += flee_dir.normalize() * 1.0,
+                };
+            }
             CurrentBehaviour::Idle => {
                 let r1: f64 = rng.random_range(-1.0..1.0);
                 let r2: f64 = rng.random_range(-1.0..1.0);
@@ -82,7 +115,7 @@ impl Organism {
                 if mov.length_squared() > 0.0001 {
                     self.pos += mov.normalize() * self.speed * (movement * 2.0);
                 }
-            },
+            }
         }
     }
     fn clear_vision(&mut self) {
@@ -100,7 +133,7 @@ impl Organism {
                     self.current_behaviour = CurrentBehaviour::Idle;
                     return;
                 }
-    
+
                 let mut flee_dir = DVec2::ZERO;
                 for threat in &self.visible_creatures {
                     let to_threat = *threat - self.pos;
@@ -108,17 +141,21 @@ impl Organism {
                         flee_dir -= to_threat.normalize();
                     }
                 }
-    
+
                 if flee_dir.length_squared() > 0.0001 {
                     self.current_behaviour = CurrentBehaviour::Active(flee_dir.normalize());
                 } else {
                     self.current_behaviour = CurrentBehaviour::Idle;
                 }
-            },
-    
+            }
+
             BehaviourArchetypes::Hunter => {
-                let closest_prey: Option<DVec2> = self.visible_creatures.iter().min_by(|a, b| a.distance(self.pos).total_cmp(&b.distance(self.pos))).copied();
-    
+                let closest_prey: Option<DVec2> = self
+                    .visible_creatures
+                    .iter()
+                    .min_by(|a, b| a.distance(self.pos).total_cmp(&b.distance(self.pos)))
+                    .copied();
+
                 if let Some(target) = closest_prey {
                     let raw_dir = target - self.pos;
                     if raw_dir.length_squared() > 0.0001 {
@@ -142,5 +179,5 @@ pub enum BehaviourArchetypes {
 #[derive(Debug, PartialEq, Clone)]
 pub enum CurrentBehaviour {
     Active(DVec2),
-    Idle
+    Idle,
 }
